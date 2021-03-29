affdc359c29dacb78ef9e10e68b70812146cf866
Edouard Munck <edouard@go-text.me>
2021-03-29 14:39:36 -0700

    upodate TextMeStudioLib.xcframework

---
+++ iOS/TextMeStudioLib.xcframework/ios-x86_64-simulator/TextMeStudioLib.framework/Headers/TextMeStudioLib.h
@@ -0,0 +1,18 @@
+//
+//  TextMeStudioLib.h
+//  TextMeStudioLib
+//
+//  Created by Edouard on 3/25/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TextMeStudioLib.
+FOUNDATION_EXPORT double TextMeStudioLibVersionNumber;
+
+//! Project version string for TextMeStudioLib.
+FOUNDATION_EXPORT const unsigned char TextMeStudioLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TextMeStudioLib/PublicHeader.h>
+
+
